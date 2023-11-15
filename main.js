5e3e7d9ebb9cd53a7642b9fb30c52deb6faa4c50
luciepostiaux <lucie.postiaux@ifosup.wavre.be>
2023-11-15 15:06:41 +0100

    changes

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ const listeArticles = () => {
   for (let i = 0; i < 4; i++) {
     const articles = data[i];
     let articlePost = `
-      <a class="flex shadow-md shadow-black" href="/news/news/?id=${articles.id}">
+      <a class="flex shadow-md shadow-black" href="/fiche/index.html?id=${articles.id}">
         <img src="${articles.avatar}" class="aspect-square bg-cover bg-center h-48 ml-4 m-auto" alt="">
         <div class="p-4">
           <div class="flex justify-between mb-2"><h5 class="text-xl font-semibold">${articles.nom}</h5>
